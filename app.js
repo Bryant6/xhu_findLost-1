e090f64968c5433ca2149a640cf6eb7320b4e931
chennumberone <527455165@qq.com>
2019-08-15 09:36:29 +0800

    获取用户唯一凭证，换图标

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-//app.js
 App({
   onLaunch: function() {
-    //首先清除数据缓存
-    wx.clearStorage();
-    console.log("清楚数据缓存");
-
     wx.getSystemInfo({
       success: e => {
         this.globalData.StatusBar = e.statusBarHeight;
@@ -13,31 +8,65 @@ App({
         this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
       }
     })
-    // 获取用户信息
-    // wx.getSetting({
-    //   success: res => {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-    //       wx.getUserInfo({
-    //         success: res => {
-    //           // 可以将 res 发送给后台解码出 unionId
-    //           this.globalData.userInfo = res.userInfo
+    //是否有openid的缓存
+    var isOpenidHave = false;
+    //清除数据缓存并查看是否有openid
+    wx.getStorageInfo({
+      success: function(res) {
+        for (let i = 0; i < res.keys.length; i++) {
+          if (res.keys[i] != 'openid') {
+            wx.removeStorage({
+              key: res.keys[i]
+            })
+          } else {
+            isOpenidHave = true;
+          }
+        }
+        console.log("清楚数据缓存");
 
-    //           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //           // 所以此处加入 callback 以防止这种情况
-    //           if (this.userInfoReadyCallback) {
-    //             this.userInfoReadyCallback(res)
-    //           }
-    //         }
-    //       })
-    //     }
-    //   }
-    // })
+        if (!isOpenidHave) {
+          console.log("云获取openid")
+          // 获取用户唯一凭证
+          let openid = null;
+          let appid = "wxdcc38317dcf7db7a";
+          let secret = "f9a555e9688e1b524103b26e41f1ae3f";
+          let code = "";
+
+          wx.login({
+            success(res) {
+              code = res.code;
+              wx.request({
+                url: 'https://api.weixin.qq.com/sns/jscode2session',
+                method: 'get',
+                data: {
+                  appid: appid,
+                  secret: secret,
+                  js_code: code,
+                  grant_type: "authorization_code"
+                },
+                success(res) {
+                  openid = res.data.openid;
+                  console.log(openid)
+                  wx.setStorage({
+                    key: 'openid',
+                    data: openid,
+                  })
+                },
+                fail(res) {
+                  console.log("获取唯一凭证失败")
+                  console.log(res)
+                }
+              })
+            }
+          })
+        }
+      },
+    })
   },
   globalData: {
     userInfo: null,
     // url: 'http://mumucoder.free.idcfengye.com/'
-    url:'http://localhost:8080/',
+    url: 'http://localhost:8080/',
     // url: 'http://xiaobai.free.idcfengye.com/'
   },
 })
